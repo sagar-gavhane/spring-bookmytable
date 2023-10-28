a58dac2e35b6cf28fbede5ca3461640958dc99e8
Sagar Gavhane <sgavhane70@gmail.com>
2023-10-28 21:54:31 +0530

    feat: initial commit

---
+++ src/main/java/com/sagar/bookmytable/BookmytableApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.bookmytable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmytableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmytableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/bookmytable/BookmytableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.bookmytable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmytableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
